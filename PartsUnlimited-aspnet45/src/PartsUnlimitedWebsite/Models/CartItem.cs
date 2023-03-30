a79392a8b6b42200616d615c2b6e52cd4a9d47e9
ALARIFNOUHAILA <ALARIFNOUHAILA@example.com>
2023-03-30 12:20:15 +0000

    My commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {   
-        // My  change
+        // My first change
         [Key]
         public int CartItemId { get; set; }
 
